9adc5ce6eb05299fb42f75a8a83a8d4efa6b0409
Adithya <jhadith88@gmail.com>
2024-04-26 20:05:12 +0530

    first commit

---
+++ practice/db/index.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('');
+
+const UserSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const AdminSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const CourseSchema = new mongoose.Schema();
+
+const user = mongoose.model('User', UserSchema);
+const admin = mongoose.model('Admin', AdminSchema);
+const course = mongoose.model('Course', CourseSchema);
