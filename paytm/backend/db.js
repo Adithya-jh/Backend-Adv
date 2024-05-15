c66fb31d155a27ae6ceb6fd4cb3b7a60a1c70eb4
Adithya <jhadith88@gmail.com>
2024-05-15 20:30:08 +0530

    added db and account

--- paytm/backend/db.js
+++ paytm/backend/db.js
@@ -4,13 +4,52 @@ mongoose.connect(
   'mongodb+srv://JHA:adith%402003@cluster0.0tkqt.mongodb.net/paytm'
 );
 
-const schema = mongoose.Schema({
-  userName: String,
-  password: String,
-  firstName: String,
-  lastName: String,
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
 });
 
-const User = mongoose.model('User', schema);
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: 'User',
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = { User };
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User,
+  Account,
+};
