88deda9e32717fdeb23f705ecf33a80058cbd6fe
Adithya <jhadith88@gmail.com>
2024-05-28 17:41:28 +0530

    ddd

--- CustomHooks/custom-hooks/src/App.jsx
+++ CustomHooks/custom-hooks/src/App.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+
 import './App.css';
 
 function App() {
   return (
     <>
-      {/* <MyComponent /> */}
+      <MyComponent />
       <MyComponent2 />
     </>
   );
