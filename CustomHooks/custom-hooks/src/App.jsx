f7f8d634d202f5353da94f95e735ef15f7a5068f
Adithya <jhadith88@gmail.com>
2024-05-29 13:58:57 +0530

    custom hook cont

--- CustomHooks/custom-hooks/src/App.jsx
+++ CustomHooks/custom-hooks/src/App.jsx
@@ -1,50 +1,82 @@
-import React, { useState } from 'react';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+function useTodo() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    axios.get('https://sum-server.100xdevs.com/todos').then((res) => {
+      setTodos(res.data.todos);
+      console.log(todos);
+    });
+  }, []);
+
+  return todos;
+}
+
 function App() {
+  const todos = useTodo();
   return (
     <>
-      <MyComponent />
-      <MyComponent2 />
+      {/* <MyComponent />
+      <MyComponent2 /> */}
+      <div>
+        {todos.map((todo) => {
+          <div>
+            {todo.title}
+            <br />
+            {todo.description}
+          </div>;
+        })}
+      </div>
     </>
   );
 }
 
-const MyComponent = () => {
-  const [count, setCount] = useState(0);
-  const incrementCount = () => {
-    setCount(count + 1);
-  };
-  return (
-    <div>
-      <p>{count}</p>
-      <button onClick={incrementCount}>Increment</button>
-    </div>
-  );
-};
+// function Track({ todo }) {
+//   return (
+//     <div>
+//       {todo.title}
+//       <br />
+//       {todo.description}
+//     </div>
+//   );
+// }
+
+// const MyComponent = () => {
+//   const [count, setCount] = useState(0);
+//   const incrementCount = () => {
+//     setCount(count + 1);
+//   };
+//   return (
+//     <div>
+//       <p>{count}</p>
+//       <button onClick={incrementCount}>Increment</button>
+//     </div>
+//   );
+// };
 
 //class based component
 //this is how react was coded before hooks and functional componnets , untill 2019
 
-class MyComponent2 extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-  }
+// class MyComponent2 extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
 
-  incrementCount = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
+//   incrementCount = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   };
 
-  render() {
-    return (
-      <div>
-        <p>{this.state.count}</p>
-        <button onClick={this.incrementCount}>Increment</button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.state.count}</p>
+//         <button onClick={this.incrementCount}>Increment</button>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
