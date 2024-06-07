2b704cad67d66d5d3693af890d36714143c9f784
Adithya <jhadith88@gmail.com>
2024-06-07 23:37:39 +0530

    update b\

--- sql/src/joins/advance-1.ts
+++ sql/src/joins/advance-1.ts
@@ -1,21 +1,21 @@
-import { getClient } from "../utils";
+import { getClient } from '../utils';
 
 // Get all todos for a give user
 // This needs to ensure that every user comes atleast once
 async function getUserAndTodosWithJoin(userId: number) {
-    const client = await getClient();
+  const client = await getClient();
 
-    const joinQuery = `
+  const joinQuery = `
         SELECT users.*, todos.title, todos.description, todos.done
         FROM users
         LEFT JOIN todos ON users.id = todos.user_id
         WHERE users.id = $1;
     `;
 
-    const res = await client.query(joinQuery, [userId]);
-    const results = res.rows;
+  const res = await client.query(joinQuery, [userId]);
+  const results = res.rows;
 
-    console.log("User and Todos:", results);
+  console.log('User and Todo are:', results);
 }
 
-getUserAndTodosWithJoin(1)
+getUserAndTodosWithJoin(1);
