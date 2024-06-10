44b88140143bf7ca23eeb20098aba23ef50fb5cb
Adithya <jhadith88@gmail.com>
2024-06-10 22:15:59 +0530

    csd

--- sql/src/joins/advance-2.ts
+++ sql/src/joins/advance-2.ts
@@ -1,21 +1,21 @@
-import { getClient } from "../utils";
+import { getClient } from '../utils';
 
 // Get all todos for a give user
-// This shouldnt return a row if no todos exist for the user
+// This shouldnt return a row if no todos exist for the users
 async function getUserAndTodosWithJoin(userId: number) {
-    const client = await getClient();
+  const client = await getClient();
 
-    const joinQuery = `
+  const joinQuery = `
         SELECT users.*, todos.title, todos.description, todos.done
         FROM users
         JOIN todos ON users.id = todos.user_id
         WHERE users.id = $1;
     `;
 
-    const res = await client.query(joinQuery, [userId]);
-    const results = res.rows;
+  const res = await client.query(joinQuery, [userId]);
+  const results = res.rows;
 
-    console.log("User and Todos:", results);
+  console.log('User and Todos:', results);
 }
 
-getUserAndTodosWithJoin(5)
+getUserAndTodosWithJoin(5);
