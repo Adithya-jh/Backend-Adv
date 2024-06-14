f40f7fccd4f0ed4820c3b4b1f0d493183fe82b4c
Adithya <jhadith88@gmail.com>
2024-06-14 20:19:39 +0530

    updation in prisma

---
+++ prisma/src/update-user.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.users.update({
+    where: {
+      id: 1,
+    },
+    data: {
+      email: 'jha234@gmail.com',
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    console.log('done');
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
