891d3ae0f6779d5104bfe1bc69d730e188199c3d
Adithya <jhadith88@gmail.com>
2024-06-15 18:58:26 +0530

    startedout serverless functions

---
+++ svless/src/index.ts
@@ -0,0 +1,29 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+
+		if (request.method === 'GET') {
+			return Response.json({
+				message: 'Hello from Cloudflare Workers! , You sent the get workers',
+			});
+		} else {
+			return Response.json({
+				message: 'You did not get any msg',
+			});
+		}
+	},
+} satisfies ExportedHandler<Env>;
