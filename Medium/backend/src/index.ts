31f54445f0d1bf7f3ff29cf950466c30399e1e65
Adithya <jhadith88@gmail.com>
2024-06-27 18:44:18 +0530

    setted up backend

--- Medium/backend/src/index.ts
+++ Medium/backend/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({
+    // datasourceUrl: env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-export default app
+  return c.text('Hello Hono!');
+});
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!');
+});
+
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!');
+});
+
+export default app;
