4dbfd8851990e935643d65e85a3d97cf1e2a2d60
Adithya <jhadith88@gmail.com>
2024-06-29 13:39:09 +0530

    add jwt

--- Medium/backend/src/index.ts
+++ Medium/backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
+import { decode, sign, verify } from 'hono/jwt';
 
 const app = new Hono<{
   Bindings: {
