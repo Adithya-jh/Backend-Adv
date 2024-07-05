6aeb0832db6b46eb856eb3c5780c357a085456bd
Adithya <jhadith88@gmail.com>
2024-07-05 13:00:23 +0530

    added zod

--- Medium/backend/src/routes/user.ts
+++ Medium/backend/src/routes/user.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
 import { sign, verify } from 'hono/jwt';
+import { signupInput } from 'jha.medium-common';
 
 export const userRouter = new Hono<{
   Bindings: {
@@ -16,6 +17,14 @@ userRouter.post('/signup', async (c) => {
   }).$extends(withAccelerate());
 
   const body = await c.req.json();
+
+  const { success } = signupInput.safeParse(body);
+  if (!success) {
+    c.status(411);
+    return c.json({
+      error: 'Invalid input',
+    });
+  }
   const user = await prisma.user.create({
     data: {
       email: body.email,
