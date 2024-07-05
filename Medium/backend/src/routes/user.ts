601ab27de0a6c284c556f4f3320e533b97c448fe
Adithya <jhadith88@gmail.com>
2024-07-05 13:05:43 +0530

    jk

--- Medium/backend/src/routes/user.ts
+++ Medium/backend/src/routes/user.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client/edge';
 import { withAccelerate } from '@prisma/extension-accelerate';
-import { sign, verify } from 'hono/jwt';
+import { sign } from 'hono/jwt';
 import { signupInput } from 'jha.medium-common';
 
 export const userRouter = new Hono<{
