e06086555ef893828ebacba142f3f08ad7ff9598
Adithya <jhadith88@gmail.com>
2024-07-16 18:30:07 +0530

    added db

--- paytm/apps/paytm-client/app/page.tsx
+++ paytm/apps/paytm-client/app/page.tsx
@@ -1,4 +1,7 @@
+import { PrismaClient } from '@repo/db/client';
+
 export default function Home() {
+  const client = new PrismaClient();
   return (
     <div className="">
       <div className="text-red-700">Hi</div>
