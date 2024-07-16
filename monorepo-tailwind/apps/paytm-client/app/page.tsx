6b6b9c8faf7718823ee74e3f14f6fa1f1d49460a
Adithya <jhadith88@gmail.com>
2024-07-16 20:30:16 +0530

    refactor

--- monorepo-tailwind/apps/paytm-client/app/page.tsx
+++ monorepo-tailwind/apps/paytm-client/app/page.tsx
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@repo/db/client';
 
 export default function Home() {
-  const client = new PrismaClient();
+  // const client = new PrismaClient();
   return (
     <div className="">
       <div className="text-red-700">Hi</div>
