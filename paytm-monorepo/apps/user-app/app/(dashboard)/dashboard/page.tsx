bb90533aaf4deb35bdec41f06ecda52ee950f682
Adithya <jhadith88@gmail.com>
2024-07-17 19:54:37 +0530

    paytm comp

---
+++ paytm-monorepo/apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function page() {
+  return <div>page from dashboard</div>;
+}
+
+export default page;
---
+++ paytm-monorepo/apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function page() {
+  return <div>page from transaction</div>;
+}
+
+export default page;
