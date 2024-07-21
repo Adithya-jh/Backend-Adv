6da5734e9e27a6e3682323b331f8b24c73d02a01
Adithya <jhadith88@gmail.com>
2024-07-21 22:52:27 +0530

    created a websocket

---
+++ Websockets/src/index.ts
@@ -0,0 +1,23 @@
+import WebSocket, { WebSocketServer } from 'ws';
+
+import express from 'express';
+//when-ever we are creating websocket connection ->
+//we are creating http connection only - later it gets upgraded into a websocket connection.
+const app = express();
+const httpServer = app.listen(8080);
+
+const wss = new WebSocketServer({ server: httpServer });
+
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+
+  ws.send('Hello! Message From Server!!');
+});
