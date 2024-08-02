c08fbce9e755025aff495c7c202cf0df56116e09
Jayhari Aditiya <jhadith88@gmail.com>
2024-08-02 16:46:04 +0530

    introduced jest

---
+++ Testing/src/tests/index.test.ts
@@ -0,0 +1,8 @@
+import {describe, expect, it} from '@jest/globals';
+import {sum} from '../index';
+
+describe('sum module', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+});
