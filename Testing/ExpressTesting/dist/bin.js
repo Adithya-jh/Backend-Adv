2c4ab23b8b7a9f8566efcf1a1a121789d735891d
Jayhari Aditiya <jhadith88@gmail.com>
2024-08-02 19:27:58 +0530

    testing using supertest

---
+++ Testing/ExpressTesting/dist/bin.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const _1 = require(".");
+_1.app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
