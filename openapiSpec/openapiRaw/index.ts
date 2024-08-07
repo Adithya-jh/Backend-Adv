7a6293ba6422978ff00befd470f281e3caa52a10
Jayhari Aditiya <jhadith88@gmail.com>
2024-08-07 14:30:36 +0530

    refactor

--- openapiSpec/openapiRaw/index.ts
+++ openapiSpec/openapiRaw/index.ts
@@ -3,30 +3,31 @@ import swaggerUi from 'swagger-ui-express';
 
 import { openapiSpec } from './openapispec';
 
-
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
 let users = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Doe' }
+  { id: 1, name: 'John Doe' },
+  { id: 2, name: 'Jane Doe' },
 ];
 
 app.get('/users', (req, res) => {
-    const { name } = req.query;
+  const { name } = req.query;
 
-    if (name) {
-        const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toString().toLowerCase()));
-        res.json(filteredUsers);
-    } else {
-        res.json(users);
-    }
+  if (name) {
+    const filteredUsers = users.filter((user) =>
+      user.name.toLowerCase().includes(name.toString().toLowerCase())
+    );
+    res.json(filteredUsers);
+  } else {
+    res.json(users);
+  }
 });
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
